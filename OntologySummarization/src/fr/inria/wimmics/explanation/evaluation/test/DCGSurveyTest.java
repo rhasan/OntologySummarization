da837f7f6ee3e9120592d9aacec7922728a1ad05
Rakebul Hasan <hasan.rakebul@inria.fr>
2013-02-12 15:14:55 +0100

    dcg survey update

--- OntologySummarization/src/fr/inria/wimmics/explanation/evaluation/test/DCGSurveyTest.java
+++ OntologySummarization/src/fr/inria/wimmics/explanation/evaluation/test/DCGSurveyTest.java
@@ -1,16 +1,11 @@
 package fr.inria.wimmics.explanation.evaluation.test;
 
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+
 import java.io.IOException;
-import java.util.List;
-import java.util.Scanner;
 
 import org.junit.Test;
 
-import au.com.bytecode.opencsv.CSVReader;
 
 public class DCGSurveyTest {
 	//files/evaluation/dcg/survey/inf1/results-survey31573.csv
@@ -20,7 +15,7 @@ public class DCGSurveyTest {
 	@Test
 	public void testCSVInput() throws IOException {
 		System.out.println("Hello there!");
-		DCGSurveyEntry en = new DCGSurveyEntry();
+		DCGSurveyEntryProcessor en = new DCGSurveyEntryProcessor();
 		en.setValues("files/evaluation/dcg/survey/inf1/results-survey31573.csv");
 		en.printValues();
 	}
